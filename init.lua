85b2c089241cbe5df83d96bbcefc7d35607f0153
Yassine Zouggari <git@zoug.fr>
2025-03-10 16:15:07 +0100

    Display tabline

--- init.lua
+++ init.lua
@@ -789,6 +789,9 @@ require('lazy').setup({
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function() return '%2l:%-2v' end
 
+      -- CUSTOM: add tabline
+      require('mini.tabline').setup()
+
       -- ... and there is more!
       --  Check out: https://github.com/nvim-mini/mini.nvim
     end,
