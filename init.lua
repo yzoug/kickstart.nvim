e155b6249fa7390b3f6dea1d4efda7f0f2e7e022
Yassine Zouggari <git@zoug.fr>
2025-11-23 11:53:58 +0100

    Disable guess-indent plugin
    A little buggy in my experience

--- init.lua
+++ init.lua
@@ -195,7 +195,7 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
-  { 'NMAC427/guess-indent.nvim', opts = {} },
+  --{ 'NMAC427/guess-indent.nvim', opts = {} },
 
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
