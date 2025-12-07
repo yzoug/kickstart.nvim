6144ecadfbb7158df50397a5a762d96a0b497e31
Yassine Zouggari <git@zoug.fr>
2025-12-07 10:04:59 +0100

    Add pyright, prettier and shfmt config

--- init.lua
+++ init.lua
@@ -535,7 +535,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         biome = {},
         rust_analyzer = {},
         ansiblels = {},
@@ -593,7 +593,9 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        -- You can add other tools here that you want Mason to install
+        'stylua', -- Used to format Lua code
+        'prettier',
+        'shfmt',
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -640,7 +642,12 @@ require('lazy').setup({
       formatters_by_ft = {
         -- rust = { 'rustfmt' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        --python = { 'ruff', 'prettier' },
+        python = { 'prettier' },
+        sh = { 'shfmt' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
