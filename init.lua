ca76933e3c06d38bc4dcacc07fec04be99c562b6
Yassine Zouggari <git@zoug.fr>
2025-12-07 10:03:53 +0100

    Enable inlay hints by default for LSPs supporting it

--- init.lua
+++ init.lua
@@ -514,6 +514,8 @@ require('lazy').setup({
           --
           -- This may be unwanted, since they displace some of your code
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            -- CUSTOM: trying out always enabling inlay hints when possible
+            vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
